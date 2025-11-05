557522005979d2d74d63d710470f6a50e265adcb
Rohita Bollam <157587643+Rohita25@users.noreply.github.com>
2025-11-05 18:38:06 +0530

    Remove base folder configuration from vite.config.ts
    Removed base configuration for the Vite project.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio/', // 👈 folder name of your repo
   plugins: [
     react({
       babel: {
